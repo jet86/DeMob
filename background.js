ac32415ad343ab6464e22dfc0af01c4dd21b6868
Tim Jones <tim@timjones.id.au>
2019-04-05 14:20:17 +1100

    Initial stage of working across multiple domains

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
 var host = "https://www.reuters.com/";
+var hostList = {
+    "*://mobile.reuters.com/*" : "https://www.reuters.com/",
+    "*://mobile.twitter.com/*" : "https://twitter.com/"
+}
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
          return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
     },
     {
         urls: [
-            "*://mobile.reuters.com/*"
+            "*://mobile.reuters.com/*",
+            "*://mobile.twitter.com/*"
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
