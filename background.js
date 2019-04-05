ca932934d5ca38120c2aeed38004ef520245fa4b
Tim Jones <tim@timjones.id.au>
2019-04-05 11:38:53 +1100

    Added initial background script
    Basic redirection for a single domain (Reuters)

---
+++ background.js
@@ -0,0 +1,13 @@
+var host = "https://www.reuters.com/";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://mobile.reuters.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
