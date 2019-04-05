e24419259b56244a998f0d7313ca3e91381ea72c
Tim Jones <tim@timjones.id.au>
2019-04-05 15:09:29 +1100

    Redirecting based on dynamic hostList

--- background.js
+++ background.js
@@ -1,18 +1,21 @@
-var host = "https://www.reuters.com/";
 var hostList = {
-    "*://mobile.reuters.com/*" : "https://www.reuters.com/",
-    "*://mobile.twitter.com/*" : "https://twitter.com/"
+    "*://mobile.twitter.com/*" : "https://twitter.com",
+    "*://mobile.reuters.com/*" : "https://www.reuters.com"
 }
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+        for (const mHost in hostList) {
+            var mHostTrimmed = mHost.split('/');
+            var rEx = new RegExp("^https?:\/\/" + mHostTrimmed[2])
+            if(details.url.match(rEx)) {
+                console.log("Host matched: " + mHost + " Redirecting to: " + hostList[mHost]);
+                var host = hostList[mHost];
+            }
+        }
+        return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
     },
     {
-        urls: [
-            "*://mobile.reuters.com/*",
-            "*://mobile.twitter.com/*"
-        ],
-        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+        urls: Object.keys(hostList)
     },
     ["blocking"]
 );
